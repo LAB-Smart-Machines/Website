d432dab82e3aa4f882cea1354f607d116eff4e0d
Vanessa <vahurtad@ucsc.edu>
2020-02-12 15:33:31 -0800

    cambiando a componentes

--- src/app/pages/proyecto.js
+++ src/app/pages/proyecto.js
@@ -1,6 +1,10 @@
+import * as React from "react";
+
 import App from "../components/App";
+import GraphQLProveedor from "../components/GraphQLProveedor";
+
 import { useRouter } from "next/router";
-import Test from "./test";
+import { GraphQL, GraphQLProvider } from "graphql-react";
 
 const Page = () => {
   const router = useRouter();
@@ -11,7 +15,7 @@ const Page = () => {
         <div className="w-container">
           <h1 className="heading-3">{router.query.titulo}</h1>
           <div className="divider grey"></div>
-          <Test repoString={router.query.titulo} />
+          <GQL repoString={router.query.titulo} />
         </div>
       </div>
     </App>
@@ -19,3 +23,16 @@ const Page = () => {
 };
 
 export default Page;
+
+// Zero config GraphQL client that manages the cache.
+const graphql = new GraphQL();
+const GQL = ({ queryString, repoString }) => {
+  return (
+    <GraphQLProvider graphql={graphql}>
+      <GraphQLProveedor
+        queryString="LAB-Smart-Machines"
+        repoString={repoString}
+      />
+    </GraphQLProvider>
+  );
+};
